5a4f98f57887c2091033c671933d329f05c59d42
didi <git@d10r.net>
2024-10-17 19:33:54 +0200

    complete core feature set

--- src/DashboardMacro.sol
+++ src/DashboardMacro.sol
@@ -21,15 +21,24 @@ using AmountFormatter for uint256;
 /*
 TODO:
 -[X] Implement "Ownable" for reverse ENS
--[ ] implement all "core actions"
+-[X] implement all "core actions"
 */
 contract DashboardMacro is MacroBase712, Ownable {
     uint8 constant ACTION_CREATE_FLOW = 1;
-    uint8 constant ACTION_UPGRADE = 2;
+    uint8 constant ACTION_UPDATE_FLOW = 2;
+    uint8 constant ACTION_DELETE_FLOW = 3;
+    uint8 constant ACTION_UPGRADE = 4;
+    uint8 constant ACTION_DOWNGRADE = 5;
+    uint8 constant ACTION_APPROVE = 6;
+    uint8 constant ACTION_TRANSFER = 7;
 
     bytes32 constant public TYPEHASH_CREATE_FLOW = keccak256(bytes("SuperfluidCreateFlow(string action,address token,address receiver,int96 flowRate)"));
+    bytes32 constant public TYPEHASH_UPDATE_FLOW = keccak256(bytes("SuperfluidUpdateFlow(string action,address token,address receiver,int96 flowRate)"));
+    bytes32 constant public TYPEHASH_DELETE_FLOW = keccak256(bytes("SuperfluidDeleteFlow(string action,address token,address sender,address receiver)"));
     bytes32 constant public TYPEHASH_UPGRADE = keccak256(bytes("SuperfluidUpgrade(string action,address token,uint256 amount)"));
-
+    bytes32 constant public TYPEHASH_DOWNGRADE = keccak256(bytes("SuperfluidDowngrade(string action,address token,uint256 amount)"));
+    bytes32 constant public TYPEHASH_APPROVE = keccak256(bytes("SuperfluidApprove(string action,address token,address spender,uint256 amount)"));
+    bytes32 constant public TYPEHASH_TRANSFER = keccak256(bytes("SuperfluidTransfer(string action,address token,address receiver,uint256 amount)"));
     address payable immutable FEE_RECEIVER;
     uint256 immutable FEE_AMOUNT;
     IConstantFlowAgreementV1 _cfa;
@@ -46,7 +55,7 @@ contract DashboardMacro is MacroBase712, Ownable {
     }
 
     function _getActions() internal pure override returns (Action[] memory) {
-        Action[] memory actions = new Action[](2);
+        Action[] memory actions = new Action[](7);
         actions[0] = Action({
             actionCode: ACTION_CREATE_FLOW,
             buildOperations: _buildOperationsForCreateFlow,
@@ -54,9 +63,39 @@ contract DashboardMacro is MacroBase712, Ownable {
             postCheck: _noPostCheck
         });
         actions[1] = Action({
+            actionCode: ACTION_UPDATE_FLOW,
+            buildOperations: _buildOperationsForUpdateFlow,
+            getDigest: _getDigestForUpdateFlow,
+            postCheck: _noPostCheck
+        });
+        actions[2] = Action({
+            actionCode: ACTION_DELETE_FLOW,
+            buildOperations: _buildOperationsForDeleteFlow,
+            getDigest: _getDigestForDeleteFlow,
+            postCheck: _noPostCheck
+        });
+        actions[3] = Action({
             actionCode: ACTION_UPGRADE,
             buildOperations: _buildOperationsForUpgrade,
             getDigest: _getDigestForUpgrade,
+            postCheck: _noPostCheck
+        });
+        actions[4] = Action({
+            actionCode: ACTION_DOWNGRADE,
+            buildOperations: _buildOperationsForDowngrade,
+            getDigest: _getDigestForDowngrade,
+            postCheck: _noPostCheck
+        });
+        actions[5] = Action({
+            actionCode: ACTION_APPROVE,
+            buildOperations: _buildOperationsForApprove,
+            getDigest: _getDigestForApprove,
+            postCheck: _noPostCheck
+        });
+        actions[6] = Action({
+            actionCode: ACTION_TRANSFER,
+            buildOperations: _buildOperationsForTransfer,
+            getDigest: _getDigestForTransfer,
             postCheck: _noPostCheck
         });
 
@@ -128,6 +167,124 @@ contract DashboardMacro is MacroBase712, Ownable {
         );
     }
 
+    // ACTION_UPDATE_FLOW
+
+    struct UpdateFlowParams {
+        ISuperToken superToken;
+        address receiver;
+        int96 flowRate;
+    }
+
+    function _buildOperationsForUpdateFlow(ISuperfluid host, bytes memory actionParams, address msgSender)
+        internal view returns (ISuperfluid.Operation[] memory operations)
+    {
+        UpdateFlowParams memory p = abi.decode(actionParams, (UpdateFlowParams));
+
+        operations = new ISuperfluid.Operation[](1);
+        operations[0] = ISuperfluid.Operation({
+            operationType: BatchOperation.OPERATION_TYPE_SUPERFLUID_CALL_AGREEMENT,
+            target: address(_cfa),
+            data: abi.encode(
+                abi.encodeCall(
+                    _cfa.updateFlow,
+                    (p.superToken, p.receiver, p.flowRate, new bytes(0))
+                ),
+                new bytes(0) // userdata
+            )
+        });
+    }
+
+    function _getDigestForUpdateFlow(bytes memory actionParams, bytes32 lang)
+        internal view returns (bytes32 digest)
+    {
+        UpdateFlowParams memory params = abi.decode(actionParams, (UpdateFlowParams));
+        (, , digest) = encode712UpdateFlow(lang, params);
+    }
+
+    function encode712UpdateFlow(bytes32 lang, UpdateFlowParams memory p)
+        public view
+        returns (string memory message, bytes memory paramsToProvide, bytes32 digest)
+    {
+        if (lang == "en") {
+            message = string(abi.encodePacked("Update flow to ", p.flowRate.toFlowRatePerDay(), " ", p.superToken.symbol(), "/day"));
+        } else revert UnsupportedLanguage();
+
+        paramsToProvide = abi.encode(
+            ACTION_UPDATE_FLOW,
+            lang,
+            abi.encode(p.superToken, p.receiver, p.flowRate)
+        );
+
+        digest = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    TYPEHASH_UPDATE_FLOW,
+                    keccak256(bytes(message)),
+                    p.superToken, p.receiver, p.flowRate
+                )
+            )
+        );
+    }
+
+    // ACTION_DELETE_FLOW
+
+    struct DeleteFlowParams {
+        ISuperToken superToken;
+        address sender;
+        address receiver;
+    }
+
+    function _buildOperationsForDeleteFlow(ISuperfluid host, bytes memory actionParams, address msgSender)
+        internal view returns (ISuperfluid.Operation[] memory operations)
+    {
+        DeleteFlowParams memory p = abi.decode(actionParams, (DeleteFlowParams));
+
+        operations = new ISuperfluid.Operation[](1);
+        operations[0] = ISuperfluid.Operation({
+            operationType: BatchOperation.OPERATION_TYPE_SUPERFLUID_CALL_AGREEMENT,
+            target: address(_cfa),
+            data: abi.encode(
+                abi.encodeCall(
+                    _cfa.deleteFlow,
+                    (p.superToken, p.sender, p.receiver, new bytes(0))
+                ),
+                new bytes(0) // userdata
+            )
+        });
+    }
+
+    function _getDigestForDeleteFlow(bytes memory actionParams, bytes32 lang)
+        internal view returns (bytes32 digest)
+    {
+        DeleteFlowParams memory params = abi.decode(actionParams, (DeleteFlowParams));
+        (, , digest) = encode712DeleteFlow(lang, params);
+    }
+
+    function encode712DeleteFlow(bytes32 lang, DeleteFlowParams memory p)
+        public view
+        returns (string memory message, bytes memory paramsToProvide, bytes32 digest)
+    {
+        if (lang == "en") {
+            message = string(abi.encodePacked("Delete flow of ", p.superToken.symbol()));
+        } else revert UnsupportedLanguage();
+
+        paramsToProvide = abi.encode(
+            ACTION_DELETE_FLOW,
+            lang,
+            abi.encode(p.superToken, p.sender, p.receiver)
+        );
+
+        digest = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    TYPEHASH_DELETE_FLOW,
+                    keccak256(bytes(message)),
+                    p.superToken, p.sender, p.receiver
+                )
+            )
+        );
+    }
+
     // ACTION_UPGRADE
 
     struct UpgradeParams {
@@ -176,6 +333,165 @@ contract DashboardMacro is MacroBase712, Ownable {
                     TYPEHASH_UPGRADE,
                     keccak256(bytes(message)),
                     p.superToken, p.amount
+                )
+            )
+        );
+    }
+
+    // ACTION_DOWNGRADE
+
+    struct DowngradeParams {
+        ISuperToken superToken;
+        uint256 amount;
+    }
+
+    function _buildOperationsForDowngrade(ISuperfluid host, bytes memory actionParams, address msgSender)
+        internal view returns (ISuperfluid.Operation[] memory operations)
+    {
+        DowngradeParams memory p = abi.decode(actionParams, (DowngradeParams));
+
+        operations = new ISuperfluid.Operation[](1);
+        operations[0] = ISuperfluid.Operation({
+            operationType: BatchOperation.OPERATION_TYPE_SUPERTOKEN_DOWNGRADE,
+            target: address(p.superToken),
+            data: abi.encode(p.amount)
+        });
+    }
+
+    function _getDigestForDowngrade(bytes memory actionParams, bytes32 lang)
+        internal view returns (bytes32 digest)
+    {
+        DowngradeParams memory params = abi.decode(actionParams, (DowngradeParams));
+        (, , digest) = encode712Downgrade(lang, params);
+    }
+
+    function encode712Downgrade(bytes32 lang, DowngradeParams memory p)
+        public view
+        returns (string memory message, bytes memory paramsToProvide, bytes32 digest)
+    {
+        address underlyingToken = p.superToken.getUnderlyingToken();
+        if (lang == "en") {
+            message = string(abi.encodePacked("Downgrade ", p.amount.toHumanReadable(), " ", p.superToken.symbol(), " to ", IERC20Metadata(underlyingToken).symbol()));
+        } else revert UnsupportedLanguage();
+
+        paramsToProvide = abi.encode(
+            ACTION_DOWNGRADE,
+            lang,
+            abi.encode(p.superToken, p.amount)
+        );
+
+        digest = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    TYPEHASH_DOWNGRADE,
+                    keccak256(bytes(message)),
+                    p.superToken, p.amount
+                )
+            )
+        );
+    }
+
+    // ACTION_APPROVE
+
+    struct ApproveParams {
+        ISuperToken superToken;
+        address spender;
+        uint256 amount;
+    }
+
+    function _buildOperationsForApprove(ISuperfluid host, bytes memory actionParams, address msgSender)
+        internal view returns (ISuperfluid.Operation[] memory operations)
+    {
+        ApproveParams memory p = abi.decode(actionParams, (ApproveParams));
+
+        operations = new ISuperfluid.Operation[](1);
+        operations[0] = ISuperfluid.Operation({
+            operationType: BatchOperation.OPERATION_TYPE_ERC20_APPROVE,
+            target: address(p.superToken),
+            data: abi.encode(p.spender, p.amount)
+        });
+    }
+
+    function _getDigestForApprove(bytes memory actionParams, bytes32 lang)
+        internal view returns (bytes32 digest)
+    {
+        ApproveParams memory params = abi.decode(actionParams, (ApproveParams));
+        (, , digest) = encode712Approve(lang, params);
+    }
+
+    function encode712Approve(bytes32 lang, ApproveParams memory p)
+        public view
+        returns (string memory message, bytes memory paramsToProvide, bytes32 digest)
+    {
+        if (lang == "en") {
+            message = string(abi.encodePacked("Approve spender for an allowance of", p.amount.toHumanReadable(), " ", p.superToken.symbol()));
+        } else revert UnsupportedLanguage();
+
+        paramsToProvide = abi.encode(
+            ACTION_APPROVE,
+            lang,
+            abi.encode(p.superToken, p.spender, p.amount)
+        );
+
+        digest = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    TYPEHASH_APPROVE,
+                    keccak256(bytes(message)),
+                    p.superToken, p.spender, p.amount
+                )
+            )
+        );
+    }
+
+    // ACTION_TRANSFER
+
+    struct TransferParams {
+        ISuperToken superToken;
+        address receiver;
+        uint256 amount;
+    }
+
+    function _buildOperationsForTransfer(ISuperfluid host, bytes memory actionParams, address msgSender)
+        internal view returns (ISuperfluid.Operation[] memory operations)
+    {
+        TransferParams memory p = abi.decode(actionParams, (TransferParams));
+
+        operations = new ISuperfluid.Operation[](1);
+        operations[0] = ISuperfluid.Operation({
+            operationType: BatchOperation.OPERATION_TYPE_ERC20_TRANSFER_FROM    ,
+            target: address(p.superToken),
+            data: abi.encode(msgSender,p.receiver, p.amount)
+        });
+    }
+
+    function _getDigestForTransfer(bytes memory actionParams, bytes32 lang)
+        internal view returns (bytes32 digest)
+    {
+        TransferParams memory params = abi.decode(actionParams, (TransferParams));
+        (, , digest) = encode712Transfer(lang, params);
+    }
+
+    function encode712Transfer(bytes32 lang, TransferParams memory p)
+        public view
+        returns (string memory message, bytes memory paramsToProvide, bytes32 digest)
+    {
+        if (lang == "en") {
+            message = string(abi.encodePacked("Transfer ", p.amount.toHumanReadable(), " ", p.superToken.symbol()));
+        } else revert UnsupportedLanguage();
+
+        paramsToProvide = abi.encode(
+            ACTION_TRANSFER,
+            lang,
+            abi.encode(p.superToken, p.receiver, p.amount)
+        );
+
+        digest = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    TYPEHASH_TRANSFER,
+                    keccak256(bytes(message)),
+                    p.superToken, p.receiver, p.amount
                 )
             )
         );
--- test/DashboardMacro.t.sol
+++ test/DashboardMacro.t.sol
@@ -4,84 +4,153 @@ pragma solidity ^0.8.26;
 import { VmSafe } from "forge-std/Vm.sol";
 import { console } from "forge-std/Test.sol";
 import { FoundrySuperfluidTester } from "@superfluid-finance/ethereum-contracts/test/foundry/FoundrySuperfluidTester.sol";
-import { IERC20 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import { IERC20, ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import { SuperTokenV1Library } from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperTokenV1Library.sol";
 
 import { DashboardMacro } from "../src/DashboardMacro.sol";
 
+using SuperTokenV1Library for ISuperToken;
+
 contract DashboardMacroTest is FoundrySuperfluidTester {
+    int96 constant DEFAULT_FLOWRATE = 1157407407407; // 0.1 per day
+    uint256 constant FEE_AMOUNT = 1e15;
     address payable feeReceiver = payable(address(0x420));
-    uint256 feeAmount = 1e15;
+    VmSafe.Wallet signer;
+    DashboardMacro m;
 
     constructor() FoundrySuperfluidTester(5) { }
 
-    // REF: https://book.getfoundry.sh/tutorials/testing-eip712
-    function testCreateFlow712() external {
-        int96 flowRate = 1157407407407; // 0.1 per day
+    function setUp() public override {
+        super.setUp();
 
-        VmSafe.Wallet memory signer = vm.createWallet("signer");
+        signer = vm.createWallet("signer");
         console.log("signer's wallet address %s", signer.addr);
 
-        // fund the signer with native tokens and SuperTokens
+        // fund the signer with native tokens
         vm.deal(signer.addr, 1 ether);
+
+        m = new DashboardMacro(sf.host, feeReceiver, FEE_AMOUNT);
+    }
+
+    function _fundSignerWithSuperTokens(uint256 amount) internal {
         vm.startPrank(alice);
-        superToken.transfer(signer.addr, 1e18);
+        superToken.transfer(signer.addr, amount);
         vm.stopPrank();
+    }
 
-        DashboardMacro m = new DashboardMacro(sf.host, feeReceiver, feeAmount);
-
-        vm.startPrank(signer.addr);
-
-        (string memory message, bytes memory paramsToProvide, bytes32 digest) =
-            m.encode712CreateFlow("en", DashboardMacro.CreateFlowParams(superToken, bob, flowRate));
+    function _signMessage(bytes32 digest) internal returns (bytes memory signatureVRS) {
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(signer, digest);
-        console.log("message: ", message);
+        return abi.encode(v, r, s);
+    }
 
-        bytes memory params = abi.encode(paramsToProvide, abi.encode(v, r, s));
+    function _createFlow(ISuperToken token, address receiver, int96 flowRate) internal {
+        vm.startPrank(signer.addr);
+        superToken.createFlow(receiver, flowRate);
+        vm.stopPrank();
+    }
+
+    // run macro using message, paramsToProvide, digest
+    function _runMacro(uint256 feeAmount, string memory message, bytes memory paramsToProvide, bytes32 digest) internal {
+        console.log("message: ", message);
+        bytes memory signatureVRS = _signMessage(digest);
+        bytes memory params = abi.encode(paramsToProvide, signatureVRS);
 
         vm.startPrank(signer.addr);
         sf.macroForwarder.runMacro{value: feeAmount}(m, params);
         vm.stopPrank();
+    }
 
-        assertEq(feeReceiver.balance, feeAmount, "unexpected fee receiver balance");
+    // REF: https://book.getfoundry.sh/tutorials/testing-eip712
+    function testCreateFlow712() external {
+        _fundSignerWithSuperTokens(1e18);
+
+        (string memory message, bytes memory paramsToProvide, bytes32 digest) =
+            m.encode712CreateFlow("en", DashboardMacro.CreateFlowParams(superToken, bob, DEFAULT_FLOWRATE));
+
+        _runMacro(FEE_AMOUNT, message, paramsToProvide, digest);
+
+        assertEq(feeReceiver.balance, FEE_AMOUNT, "unexpected fee receiver balance");
+        assertEq(superToken.getFlowRate(signer.addr, bob), DEFAULT_FLOWRATE, "unexpected flow rate");
+    }
+
+    function testUpdateFlow712() external {
+        int96 newFlowRate = 2 * DEFAULT_FLOWRATE;
+
+        _fundSignerWithSuperTokens(1e18);
+        _createFlow(superToken, bob, DEFAULT_FLOWRATE);
+
+        (string memory message, bytes memory paramsToProvide, bytes32 digest) = m.encode712UpdateFlow("en", DashboardMacro.UpdateFlowParams(superToken, bob, newFlowRate));
+        _runMacro(0, message, paramsToProvide, digest);
+
+        assertEq(superToken.getFlowRate(signer.addr, bob), newFlowRate, "unexpected flow rate");
+    }
+
+    function testDeleteFlow712() external {
+        _fundSignerWithSuperTokens(1e18);
+
+        _createFlow(superToken, bob, DEFAULT_FLOWRATE);
+
+        (string memory message, bytes memory paramsToProvide, bytes32 digest) = m.encode712DeleteFlow("en", DashboardMacro.DeleteFlowParams(superToken, signer.addr, bob));
+        _runMacro(0, message, paramsToProvide, digest);
+
+        assertEq(superToken.getFlowRate(signer.addr, bob), 0, "unexpected flow rate");
     }
 
     function testUpgrade712() external {
-        uint256 amount = 1e17; // 0.1
-
-        VmSafe.Wallet memory signer = vm.createWallet("signer");
-        console.log("signer's wallet address %s", signer.addr);
+        uint256 amountToUpgrade = 1e17; // 0.1
 
         IERC20 underlying = IERC20(superToken.getUnderlyingToken());
-        (uint256 underlyingAmount, uint256 adjustedAmount) = superToken.toUnderlyingAmount(amount);
+        (uint256 underlyingAmount, uint256 adjustedAmount) = superToken.toUnderlyingAmount(amountToUpgrade);
         console.log("underlyingAmount", underlyingAmount);
         console.log("adjustedAmount", adjustedAmount);
 
-        // fund the signer with native tokens and SuperTokens
+        // fund the signer with underlying tokens
         vm.deal(signer.addr, 1 ether);
         vm.startPrank(alice);
         underlying.transfer(signer.addr, 1e18);
         vm.stopPrank();
 
-        DashboardMacro m = new DashboardMacro(sf.host, feeReceiver, feeAmount);
-
         vm.startPrank(signer.addr);
-
         underlying.approve(address(superToken), underlyingAmount);
-
-        (string memory message, bytes memory paramsToProvide, bytes32 digest) = m.encode712Upgrade("en", DashboardMacro.UpgradeParams(superToken, adjustedAmount));
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(signer, digest);
-        console.log("message: ", message);
-
-        bytes memory params = abi.encode(paramsToProvide, abi.encode(v, r, s));
-
-        console.log("signer balance pre:", superToken.balanceOf(signer.addr));
-
-        vm.startPrank(signer.addr);
-        sf.macroForwarder.runMacro(m, params);
         vm.stopPrank();
 
-        console.log("signer balance post:", superToken.balanceOf(signer.addr));
+        (string memory message, bytes memory paramsToProvide, bytes32 digest) = m.encode712Upgrade("en", DashboardMacro.UpgradeParams(superToken, adjustedAmount));
+        _runMacro(0, message, paramsToProvide, digest);
 
         assertEq(superToken.balanceOf(signer.addr), adjustedAmount, "unexpected SuperToken balance");
     }
+
+    function testDowngrade712() external {
+        uint256 fundingAmount = 1e18;
+        uint256 amountToDowngrade = 1e17; // 0.1
+
+        _fundSignerWithSuperTokens(fundingAmount);
+
+        (string memory message, bytes memory paramsToProvide, bytes32 digest) = m.encode712Downgrade("en", DashboardMacro.DowngradeParams(superToken, amountToDowngrade));
+        _runMacro(0, message, paramsToProvide, digest);
+
+        assertEq(superToken.balanceOf(signer.addr), fundingAmount - amountToDowngrade, "unexpected SuperToken balance");
+    }
+
+    function testApprove712() external {
+        uint256 allowanceAmount = 1e17;
+        _fundSignerWithSuperTokens(1e18);
+
+        (string memory message, bytes memory paramsToProvide, bytes32 digest) = m.encode712Approve("en", DashboardMacro.ApproveParams(superToken, bob, allowanceAmount));
+        _runMacro(0, message, paramsToProvide, digest);
+
+        assertEq(superToken.allowance(signer.addr, bob), allowanceAmount, "unexpected allowance");
+    }
+
+    function testTransfer712() external {
+        uint256 transferAmount = 1e17;
+        _fundSignerWithSuperTokens(1e18);
+
+        uint256 bobBalance = superToken.balanceOf(bob);
+        (string memory message, bytes memory paramsToProvide, bytes32 digest) = m.encode712Transfer("en", DashboardMacro.TransferParams(superToken, bob, transferAmount));
+        _runMacro(0, message, paramsToProvide, digest);
+
+        assertEq(superToken.balanceOf(signer.addr), 1e18 - transferAmount, "unexpected SuperToken balance");
+        assertEq(superToken.balanceOf(bob), bobBalance + transferAmount, "unexpected SuperToken balance");
+    }   
 }
